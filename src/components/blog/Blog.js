f116493a65f064945e79b0eb94f95a6598f1f42c
Cherie C. Malone <cheriecmalone@gmail.com>
2022-03-23 12:13:55 +0100

    styled and updated retrieval method for json data

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,28 +1,71 @@
-import React, { useContext } from 'react';
-import { BlogContext } from '../../contexts/BlogContext';
+import React from 'react';
+import blogCards from '../../data/data.json';
+import './blog.css';
+
+const Blog = () => {
+  return (
+    <div className="categories">
+      <h2 className="blogTitle"> Travel Blogs </h2>
+      <br />
+
+      {blogCards &&
+        blogCards.map((blogs) => {
+          return (
+            <div className="cards" key={blogs.id}>
+              <div className="title">
+                <strong>{blogs.title}</strong>
+                <br />
+                {blogs.category}
+                <br />
+                {blogs.description}
+                <br />
+              </div>
+
+              <div className="author">
+                {blogs.authorName}
+                <br />
+                <div>
+                  <img alt="user-icon" src={blogs.authorAvatar} />
+                </div>
+                <br />
+                {blogs.createdAt}
+                <br />
+              </div>
+
+              <br />
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Blog;
+
+// import { BlogContext } from '../../contexts/BlogContext';
 
 // import { blogContent } from '../../data/data.json';
 // import blogs from './blogs';
 
 //react context API is best used for global data that does not change frequently
 
-const Blog = () => {
-  const { blog } = useContext(BlogContext);
+// const Blog = () => {
+//   const { blog } = useContext(BlogContext);
 
-  return (
-    <section className="category">
-      <div className="grid">
-        <h1> Hello World</h1>
-        <ul className="">
-          {blog &&
-            blog.map((blog) => {
-              return <li key={blog.id}>{blog.title}</li>;
-            })}
-        </ul>
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section className="category">
+//       <div className="grid">
+//         <h1> Hello World</h1>
+//         <ul className="">
+//           {blog &&
+//             blog.map((blog) => {
+//               return <li key={blog.id}>{blog.title}</li>;
+//             })}
+//         </ul>
+//       </div>
+//     </section>
+//   );
+// };
 
 // const Blog = (props) => {
 //   console.log('blogs', blogContent);
@@ -46,4 +89,4 @@ const Blog = () => {
 //     </section>
 //   );
 // };
-export default Blog;
+// export default Blog;
