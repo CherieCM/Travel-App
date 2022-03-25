7a42bc372b1b915772b4a3f9b95e1374c3ec95c1
Cherie C. Malone <cheriecmalone@gmail.com>
2022-03-25 15:43:47 +0100

    updated styling for link and added link towards search

--- src/components/categories/CategoryList.jsx
+++ src/components/categories/CategoryList.jsx
@@ -1,12 +1,15 @@
 import './categories.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const CategoryList = ({ cover, category, description }) => {
   return (
     <section className={'categories'}>
       <div className={'grid'}>
         <img src={cover} alt={'travel pics'} />
-        <h1>{category}</h1>
+        <Link style={{ textDecoration: 'none' }} to="/search">
+          <h1>{category}</h1>
+        </Link>
         <p>{description}</p>
       </div>
     </section>
