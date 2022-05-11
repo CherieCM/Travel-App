7e11d014c3c6f22b5b91ce860f5816ebc60956ce
Cherie C. Malone <cheriecmalone@gmail.com>
2022-05-11 18:28:52 +0200

    deleted unused components. Login1, Greet, AuthBtn

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-// import axios from 'axios';
+
 //router connection setup
 import { Route, Routes } from 'react-router-dom';
 //import components
 import Header from './components/header/Header';
 import Login from './components/login/Login';
-// import Login1 from './components/login/Login1';
+
 import SignUp from './components/signup/Signup';
 import Profile from './components/Profile/profile';
 import Agency from './components/agency/Agency';
-// import Greet from './components/greet/Greet';
+
 import Categories from './components/categories/Categories';
 import Posts from './components/posts/Posts';
 import Search from './components/search/Search';
@@ -20,7 +20,7 @@ import Info from './components/Info/Info';
 import Youtube from './components/youtube/Youtube';
 import Booking from './components/booking/Booking';
 // import { BlogContext } from './contexts/BlogContext';
-// import AuthBtn from './components/authbtn/AuthBtn';
+
 //new version
 //import {SearchProvider} from "./contexts/SearchContext"
 import { UserContext } from './contexts/UserContext';
@@ -39,7 +39,6 @@ function App() {
         <Route path="/blog" element={<Blog />} />
         <Route path="/login" element={<Login />} />
 
-        {/* <Greet /> */}
         <Route path="/profile" element={<Profile />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/posts" element={<Posts />} />
